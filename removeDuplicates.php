28b5c928bd63d675e87609761b5d55bb1e374f13
zxl <18210515973@163.com>
2023-06-26 08:31:16 +0800

    原地删除数组重复元素

---
+++ removeDuplicates.php
@@ -0,0 +1,15 @@
+<?php
+function removeDuplicates($nums) {
+    $i = 0;
+    for ($j = 1; $j < count($nums); $j++) {
+        if ($nums[$j] != $nums[$i]) {
+            $i++;
+            $nums[$i] = $nums[$j];
+        }
+    }
+
+    return array_slice($nums, 0, $i+1);
+}
+
+$nums = [0,0,1,1,1,2,2,3,3,4];
+print_r(removeDuplicates($nums));
